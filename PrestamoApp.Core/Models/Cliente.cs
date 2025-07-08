4b40d9b03ff3702d3310888922c535197024710e
Ermogenes Rodriguez <theliomdjprodutions@gmai.com>
2025-07-08 09:43:11 -0400

    Actualizado PrestamoApp.core/model/Clientes

--- PrestamoApp.Core/Models/Cliente.cs
+++ PrestamoApp.Core/Models/Cliente.cs
@@ -11,13 +11,13 @@ namespace PrestamoApp.Models
         public int IdCliente { get; set; }
         [Required]
         [StringLength(20, ErrorMessage = "El nombre no puede exceder los 20 caracteres.")]
-        public string NroDocumento { get; set; }
+        public string? NroDocumento { get; set; }
         [Required]
         [StringLength(50, ErrorMessage = "El nombre no puede exceder los 50 caracteres.")]
-        public string Nombre { get; set; }
+        public string? Nombre { get; set; }
         [Required]
         [StringLength(50, ErrorMessage = "El apellido no puede exceder los 50 caracteres.")]
-        public string Apellido { get; set; }
+        public string? Apellido { get; set; }
         [Required]
         [StringLength(100, ErrorMessage = "El email no puede exceder los 100 caracteres.")]
         [EmailAddress(ErrorMessage = "El formato del email es inválido.")]
